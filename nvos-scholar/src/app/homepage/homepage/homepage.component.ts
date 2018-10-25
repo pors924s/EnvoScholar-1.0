59ac46676f5ec945014c8af81c2f83e28455b7ae
Bryan Bastida <bbast005@fiu.edu>
2018-10-25 15:26:06 -0400

    ok

--- nvos-scholar/src/app/homepage/homepage/homepage.component.ts
+++ nvos-scholar/src/app/homepage/homepage/homepage.component.ts
@@ -1,27 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from "@angular/common/http"
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  selector: "app-homepage",
+  templateUrl: "./homepage.component.html",
+  styleUrls: ["./homepage.component.css"]
 })
 export class HomepageComponent implements OnInit {
-
-  userName: String = "";
+  searchQuery: String = "";
   response: any;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
-  search(){
-   this.http.get('http://crest-cache-01.cs.fiu.edu:81/articles/article/_search?q=' + this.userName)
-   .subscribe((response) => {
-     this.response = response;
-     console.log(this.response);
-     console.log(this.userName);
-   })
+  /**
+   * This function is called whenever a user types a search query in either the homepage
+   * or the display results page. It will use an HTTP GET request to the JSON file containing
+   * all the article information. The displayresults.component.html file will use interpolation
+   * to show the results
+   */
+  search() {
+    this.http
+      .get(
+        "http://crest-cache-01.cs.fiu.edu:81/articles/article/_search?q=" +
+          this.searchQuery
+      )
+      .subscribe(response => {
+        this.response = response;
+      });
   }
 }
