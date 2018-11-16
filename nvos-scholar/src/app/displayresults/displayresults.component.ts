894f55e379c1dbfa4b4e02bf989ece4f97293323
Andrew Castillo <andrewcastillo@ADC-MacBook-Air.local>
2018-11-16 15:52:15 -0500

    Working date filter,Cross compare search engines

--- nvos-scholar/src/app/displayresults/displayresults.component.ts
+++ nvos-scholar/src/app/displayresults/displayresults.component.ts
@@ -12,6 +12,7 @@ import { isUndefined, isNullOrUndefined } from "util";
   styleUrls: ["./displayresults.component.css"]
 })
 export class DisplayresultsComponent implements OnInit {
+  //=============Inialize Variables=============//
   response: any;
   //list of articles after date filter in getDate()
   editedResponse: any;
@@ -25,14 +26,25 @@ export class DisplayresultsComponent implements OnInit {
   defaultMinYear: number = 1900;
   defaultMaxYear: number = 2018;
   x: number = 0;
+  numArticles: number = 100;
+  filter: boolean = false;
+  query: string = window.location.search.substring(1).split("=")[1];
+  specifyWebsite: number;
+
+
 
 
   constructor(private http: HttpClient, private router: Router) {}
 
+
+
+  //=======================WHEN PAGE LOADS=======================//
   ngOnInit() {
     this.search(0, null);
+    //this.academicSearch();
   }
-  numArticles: number = 100;
+
+
   //====================BEGIN filter info via JSON file====================//
   myParams = {
     "query": {
@@ -61,7 +73,9 @@ export class DisplayresultsComponent implements OnInit {
   dateDecoded = decodeURIComponent(this.dateEncoded);
   //====================END filter info via JSON file====================//
 
-  
+
+
+  //========================PULL SEARCH RESULTS FROM DATABASE========================//
   /**
    * This function is called whenever a user types a search query in either the homepage
    * or the display results page. It will use an HTTP GET request to the JSON file containing
@@ -69,10 +83,9 @@ export class DisplayresultsComponent implements OnInit {
    * to show the results
    */
   search(flag, filteredDate) {
-    var query: string = window.location.search.substring(1).split("=")[1];
     this.http
       .get(
-        "http://crest-cache-01.cs.fiu.edu:81/articles/article/_search?q="+ query + "&" + /*this.dateEncoded +*/ "&size=" + this.numArticles
+        "http://crest-cache-01.cs.fiu.edu:81/articles/article/_search?q="+ this.query + "&" + /*this.dateEncoded +*/ "&size=" + this.numArticles
         
       )
       .subscribe(response => {
@@ -92,12 +105,35 @@ export class DisplayresultsComponent implements OnInit {
       });
       if(flag != 0){
         this.finalResponse = filteredDate;
-        console.log(filteredDate);
-        console.log(this.responseLength);
       }
       
   }
+  /*
+  academicSearch() {
+    this.http
+      .get(
+        "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=" + this.query
+        
+      )
+      .subscribe(response => {
+        //Set this.response to the JSON file
+        this.response = response; 
+        console.log(this.response);
+      });
+      
+      
+  }*/
 
+  website(specifyWebsite){
+    alert("You are about to be directed to a different webpage.")
+    if(specifyWebsite == 0)
+      return "https://academic.microsoft.com/#/search?iq=%40" + this.query + "%40&q=" + this.query + "&filters=&from=0&sort=0";
+    else
+      return "https://www.semanticscholar.org/search?q=" + this.query + "&sort=relevance";
+  }
+
+
+  //========================CONCEPTS========================//
   /**
    * This function is called whenever a user clicks on a concept that they wish to add to
    * enhance their search. It will replace the search in the url with the concept and load
@@ -108,15 +144,16 @@ export class DisplayresultsComponent implements OnInit {
       "http://localhost:4200/displayresults?search=" +
       this.response.hits.hits[index]._source.keywords[0].keyword;
   }
-  /* SORT */
+
+
+  //====================SORT===================//
   //set default
   key: string = "_score";
-  //reverse: boolean = false;
   sort(key) {
     this.key = key;
-    //this.reverse = !this.reverse;
   }
 
+  //====================CITE====================//
   /**
    * This function is called when the user clicks on Cite. It will create a popup box
    * with the citation for the article.
@@ -130,7 +167,7 @@ export class DisplayresultsComponent implements OnInit {
     }
   }
 
-
+  //=================DATE FILTER=================//
   //Filter by date range, specify min year and max year
   getDate(minYear,maxYear) {
     var i: number = 0;
@@ -138,32 +175,33 @@ export class DisplayresultsComponent implements OnInit {
     var z: number = 0;
     var articleIndex: number[] = new Array(this.originalResponse.length);
 
-    if(minYear.isEmpty){
-      minYear = 1900;
-      console.log('why');
-    }
-    else if(maxYear.isEmpty){
-      maxYear = 2018;
-      console.log('Yes');
-    }
-    else if(minYear.isEmpty && maxYear.isEmpty){
+    if(minYear == '' && maxYear == ''){
       minYear = 1900;
       maxYear = 2018;
     }
+    else if(minYear == ''){
+      minYear = 1900;
+    }
+    else if(maxYear == ''){
+      maxYear = 2018;
+    }
+    else{
+      this.checkRange(minYear);
+      this.checkRange(maxYear);
+    }
+    
     
     
     for (i = 0; i < this.originalResponse.length; i++) {
       if (this.response.hits.hits[i]._source.cover_date.substr(0,4) >= minYear && this.response.hits.hits[i]._source.cover_date.substr(0,4) <= maxYear) {
         articleIndex[z] = i;
         z++;
-        //this.result[z] = this.response.hits.hits[i];
       }
     }
 
     var finalArticleIndex: number[] = new Array(z);
     var editedResponse: any[] = new Array(z);
     this.numArticles = z;
-    console.log(z);
 
     //Creates new response with only articles within the date range
     for(j = 0; j < z; j++){
@@ -172,19 +210,29 @@ export class DisplayresultsComponent implements OnInit {
       editedResponse[j] = this.response.hits.hits[finalArticleIndex[j]];
 
     }
+    if(editedResponse.length == 0){
+      alert("No results for date range: " + minYear + " to " + maxYear + ".\nPlease Specify a different range." );
+    }
+    else{
+      this.search(1, editedResponse);
+      this.filter = true;
+    }
 
-    this.search(1, editedResponse);
-    console.log(minYear, maxYear);
-    console.log(maxYear);
     
   }
 
-  
-  clearFilters(){
-    window.location.reload();
+  checkRange(year){
+    if((year < 1900 || year > 2018) && year != '' && year != null){
+      alert("Year: '" + year + "' is out of range");
+    }
   }
 
+  //==================CLEAR FILTER==================//
+  clearFilters(){
+    this.getDate(1900,2018);
+  }
 
+  //=======================ARTICLE=======================//
   /**
    * This function is called when a user clicks on an article link in display results. It sets the article index to the article_info to be called in the articleinfo
    */
@@ -196,4 +244,10 @@ export class DisplayresultsComponent implements OnInit {
     };
     this.router.navigate(["article"], navigationExtras);
   }
+  //============TEST TO CONSOLE============//
+  toConsole(){
+    console.log('You done testedededed it');
+  }
 }
+
+
