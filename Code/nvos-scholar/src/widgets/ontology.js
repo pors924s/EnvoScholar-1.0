a40f89f24530e39ccec9e0d970d9a84b9253df7a
Andrew Castillo <andrewcastillo@ADC-MacBook-Air.local>
2018-12-04 16:24:33 -0500

    code directory for final Deliverable

--- Code/nvos-scholar/src/widgets/ontology.js
+++
@@ -1,4 +0,0 @@
-var widget_tree = $("#widget_tree").NCBOTree({
-  apikey: "bcf3d9b2-a36e-4061-98e5-e119af71a496 ",
-  ontology: "ENVO"
-});
--- Code/user_database_server/models/feedBack.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-var feedBackSchema = new mongoose.Schema({
-  question: {
-    type: Array
-  }
-});
-
-mongoose.model("Feedback", feedBackSchema, "feedbacks");
